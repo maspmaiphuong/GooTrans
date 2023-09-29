6655844da464ece98a6193a5de56da7af7e891e3
Mappi <92283844+maspmaiphuong@users.noreply.github.com>
2023-09-29 16:25:17 +1000

    GooTrans source code updated

---
+++ GooTrans/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace GooTrans
+{
+    partial class GooGoo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GooGoo));
+            this.Eng = new System.Windows.Forms.Button();
+            this.Vie = new System.Windows.Forms.Button();
+            this.Num = new System.Windows.Forms.TextBox();
+            this.ReadNum = new System.Windows.Forms.TextBox();
+            this.Audio = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.HelpBton = new System.Windows.Forms.Button();
+            this.helpbox = new System.Windows.Forms.PictureBox();
+            this.helpbox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.helpbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.helpbox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Eng
+            // 
+            this.Eng.BackgroundImage = global::GooTrans.Properties.Resources.eng;
+            this.Eng.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Eng.Location = new System.Drawing.Point(24, 22);
+            this.Eng.Name = "Eng";
+            this.Eng.Size = new System.Drawing.Size(75, 40);
+            this.Eng.TabIndex = 0;
+            this.Eng.UseVisualStyleBackColor = true;
+            this.Eng.Click += new System.EventHandler(this.Eng_Click);
+            // 
+            // Vie
+            // 
+            this.Vie.BackgroundImage = global::GooTrans.Properties.Resources.vie;
+            this.Vie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Vie.Location = new System.Drawing.Point(108, 22);
+            this.Vie.Name = "Vie";
+            this.Vie.Size = new System.Drawing.Size(64, 41);
+            this.Vie.TabIndex = 1;
+            this.Vie.UseVisualStyleBackColor = true;
+            this.Vie.Click += new System.EventHandler(this.Vie_Click);
+            // 
+            // Num
+            // 
+            this.Num.BackColor = System.Drawing.Color.Aqua;
+            this.Num.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num.Location = new System.Drawing.Point(208, 30);
+            this.Num.Name = "Num";
+            this.Num.Size = new System.Drawing.Size(151, 33);
+            this.Num.TabIndex = 2;
+            this.Num.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Num_KeyPress);
+            // 
+            // ReadNum
+            // 
+            this.ReadNum.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReadNum.Location = new System.Drawing.Point(24, 85);
+            this.ReadNum.Multiline = true;
+            this.ReadNum.Name = "ReadNum";
+            this.ReadNum.Size = new System.Drawing.Size(335, 266);
+            this.ReadNum.TabIndex = 3;
+            // 
+            // Audio
+            // 
+            this.Audio.BackColor = System.Drawing.Color.Transparent;
+            this.Audio.FlatAppearance.BorderSize = 0;
+            this.Audio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Audio.Image = global::GooTrans.Properties.Resources.aaa1;
+            this.Audio.Location = new System.Drawing.Point(281, 362);
+            this.Audio.Name = "Audio";
+            this.Audio.Size = new System.Drawing.Size(78, 70);
+            this.Audio.TabIndex = 4;
+            this.Audio.TabStop = false;
+            this.Audio.UseVisualStyleBackColor = false;
+            this.Audio.Click += new System.EventHandler(this.Audio_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.Color.Transparent;
+            this.ExitButton.FlatAppearance.BorderSize = 0;
+            this.ExitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExitButton.ForeColor = System.Drawing.Color.Transparent;
+            this.ExitButton.Image = global::GooTrans.Properties.Resources.quit;
+            this.ExitButton.Location = new System.Drawing.Point(24, 355);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(74, 79);
+            this.ExitButton.TabIndex = 5;
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // HelpBton
+            // 
+            this.HelpBton.BackColor = System.Drawing.Color.Transparent;
+            this.HelpBton.FlatAppearance.BorderSize = 0;
+            this.HelpBton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HelpBton.ForeColor = System.Drawing.Color.Transparent;
+            this.HelpBton.Image = global::GooTrans.Properties.Resources.what;
+            this.HelpBton.Location = new System.Drawing.Point(150, 361);
+            this.HelpBton.Name = "HelpBton";
+            this.HelpBton.Size = new System.Drawing.Size(76, 70);
+            this.HelpBton.TabIndex = 6;
+            this.HelpBton.UseVisualStyleBackColor = false;
+            this.HelpBton.Click += new System.EventHandler(this.HelpButton_Click);
+            // 
+            // helpbox
+            // 
+            this.helpbox.BackColor = System.Drawing.Color.Transparent;
+            this.helpbox.Image = global::GooTrans.Properties.Resources.helper;
+            this.helpbox.Location = new System.Drawing.Point(-1, -1);
+            this.helpbox.Name = "helpbox";
+            this.helpbox.Size = new System.Drawing.Size(386, 453);
+            this.helpbox.TabIndex = 7;
+            this.helpbox.TabStop = false;
+            this.helpbox.Visible = false;
+            // 
+            // helpbox2
+            // 
+            this.helpbox2.BackColor = System.Drawing.Color.Transparent;
+            this.helpbox2.Image = global::GooTrans.Properties.Resources.helper2;
+            this.helpbox2.Location = new System.Drawing.Point(-1, -1);
+            this.helpbox2.Name = "helpbox2";
+            this.helpbox2.Size = new System.Drawing.Size(386, 453);
+            this.helpbox2.TabIndex = 8;
+            this.helpbox2.TabStop = false;
+            this.helpbox2.Visible = false;
+            // 
+            // GooGoo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::GooTrans.Properties.Resources.bg;
+            this.ClientSize = new System.Drawing.Size(383, 454);
+            this.Controls.Add(this.helpbox2);
+            this.Controls.Add(this.helpbox);
+            this.Controls.Add(this.HelpBton);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.Audio);
+            this.Controls.Add(this.ReadNum);
+            this.Controls.Add(this.Num);
+            this.Controls.Add(this.Vie);
+            this.Controls.Add(this.Eng);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "GooGoo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GooTrans";
+            this.Load += new System.EventHandler(this.GooGoo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.helpbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.helpbox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Eng;
+        private System.Windows.Forms.Button Vie;
+        private System.Windows.Forms.TextBox Num;
+        private System.Windows.Forms.TextBox ReadNum;
+        private System.Windows.Forms.Button Audio;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button HelpBton;
+        private System.Windows.Forms.PictureBox helpbox;
+        private System.Windows.Forms.PictureBox helpbox2;
+    }
+}
+
---
+++ GooTrans/Form1.cs
@@ -0,0 +1,481 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Net;
+using System.Web;
+using System.Media;
+using System.IO;
+
+namespace GooTrans
+{
+    public partial class GooGoo : Form
+    {
+        public GooGoo()
+        {
+            InitializeComponent();
+        }
+
+        bool eng = false;
+        int tmp = 0;
+        SpeechSynthesizer voice;
+
+        private void GooGoo_Load(object sender, EventArgs e)
+        {
+            voice = new SpeechSynthesizer();
+        }
+
+        string basetrans(char a)
+        {
+            string b;
+            b = "";
+            if (!eng)
+            {
+                switch (a)
+                {
+                    case '1':
+                        b = "một";
+                        break;
+                    case '2':
+                        b = "hai";
+                        break;
+                    case '3':
+                        b = "ba";
+                        break;
+                    case '4':
+                        b = "bốn";
+                        break;
+                    case '5':
+                        b = "năm";
+                        break;
+                    case '6':
+                        b = "sáu";
+                        break;
+                    case '7':
+                        b = "bảy";
+                        break;
+                    case '8':
+                        b = "tám";
+                        break;
+                    case '9':
+                        b = "chín";
+                        break;
+                    case '0':
+                        b = "không";
+                        break;
+                }
+            }
+            else
+            {
+                switch (a)
+                {
+                    case '1':
+                        b = "one";
+                        break;
+                    case '2':
+                        b = "two";
+                        break;
+                    case '3':
+                        b = "three";
+                            break;
+                    case '4':
+                        b = "four";
+                        break;
+                    case '5':
+                        b = "five";
+                        break;
+                    case '6':
+                        b = "six";
+                        break;
+                    case '7':
+                        b = "seven";
+                        break;
+                    case '8':
+                        b = "eight";
+                        break;
+                    case '9':
+                        b = "nine";
+                        break;
+                    case '0':
+                        b = "zero";
+                        break;
+                }
+            }
+            return b;
+        }
+
+        string Delo(string so)
+        {
+            while ((so.Length > 0) && (so[0] == '0'))
+            {
+                so = so.Remove(0, 1);
+            }
+            return so;
+        }
+
+        string befbase(char a)
+        {
+            string b;
+            b = "";
+            switch (a)
+            {
+                case '3':
+                    b = "thir";
+                    break;
+                case '4':
+                    b = "four";
+                    break;
+                case '5':
+                    b = "fif";
+                    break;
+                case '6':
+                    b = "six";
+                    break;
+                case '7':
+                    b = "seven";
+                    break;
+                case '8':
+                    b = "eigh";
+                    break;
+                case '9':
+                    b = "nine";
+                    break;
+            }
+            return b;
+        }
+
+        string BaseN(string x)
+        {
+            string y = "";
+            x = Delo(x);
+            int ll = x.Length;
+            if (!eng)
+            {
+                if (ll == 3)
+                {
+                    y = basetrans(x[0]) + " trăm";
+                    if ((x[1] == '0') && (x[2] != '0'))
+                        y = y + " lẻ " + basetrans(x[2]);
+                    else if (x[1] == '1')
+                    {
+                        y = y + " mười";
+                        if (x[2] == '5') y = y + " lăm";
+                        else if (x[2] != '0') y = y + " " + basetrans(x[2]);
+                    }
+                    else
+                    {
+                        if (x[1] != '0')
+                        {
+                            y = y + " " + basetrans(x[1]) + " mươi";
+                            if (x[2] == '5') y = y + " lăm";
+                            else if (x[2] == '1') y = y + " mốt";
+                            else if (x[2] != '0') y = y + " " + basetrans(x[2]);
+                        }
+                        else
+                        {
+                            if (x[2] != '0') y = y + " lẻ " + basetrans(x[2]);
+                        }
+                    }
+                }
+                else if (ll == 2)
+                {
+                    if (x[0] == '1')
+                    {
+                        y = y + "mười";
+                        if (x[1] == '5') y = y + " " + "lăm";
+                        else if (x[1] != '0') y = y + " " + basetrans(x[1]);
+                    }
+                    else
+                    {
+                        if (x[0] != '0')
+                        {
+                            y = y + basetrans(x[0]) + " mươi";
+                            if (x[1] == '5') y = y + " lăm";
+                            else if (x[1] == '1') y = y + " mốt";
+                            else if (x[1] != '0') y = y + " " + basetrans(x[1]);
+                        }
+                        else
+                        {
+                            if (x[1] != '0') y = y + " lẻ " + basetrans(x[1]);
+                        }
+                    }
+                }
+                else if (ll == 1) y = basetrans(x[0]);
+            }
+            else
+            {
+                y = "";
+                x = Delo(x);
+                ll = x.Length;
+                if (ll == 3)
+                {
+                    y = basetrans(x[0]) + " hundred";
+                    if ((x[1] == '0') && (x[2] != '0'))
+                        y = y + " and " + basetrans(x[2]);
+                    else if (x[1] == '1')
+                    {
+                        if (x[2] == '0') y = y + " ten";
+                        else if (x[2] == '1') y = y + " eleven";
+                        else if (x[2] == '2') y = y + " twelve";
+                        else y = y + " " + befbase(x[2]) + "teen";
+                    }
+                    else
+                    {
+                        if (x[1] != '0')
+                        {
+                            if (x[1] == '2') y = y + " twenty";
+                            else
+                            {
+                                if (x[0] != '0')
+                                    y = y + " " + befbase(x[1]) + "ty";
+                                else y = y + befbase(x[1]) + "ty";
+                            }
+                            if (x[2] != '0') y = y + " " + basetrans(x[2]);
+                        }
+                        else
+                        {
+                            if (x[2] != '0') y = y + " and " + basetrans(x[2]);
+                        }
+                    }
+                }
+                else if (ll == 2)
+                {
+                    if (x[0] == '1')
+                    {
+                        if (x[1] == '0') y = y + " ten";
+                        else if (x[1] == '1') y = y + " eleven";
+                        else if (x[1] == '2') y = y + " twelve";
+                        else y = y + " " + befbase(x[1]) + "teen";
+                    }
+                    else
+                    {
+                        if (x[0] != '0')
+                        {
+                            if (x[0] == '2') y = y + " twenty";
+                            else y = y + befbase(x[0]) + "ty";
+                            if (x[0] != '0') y = y + " " + basetrans(x[1]);
+                        }
+                        else
+                        {
+                            if (x[1] != '0') y = y + " and " + basetrans(x[1]);
+                        }
+                    }
+                }
+                else if (ll == 1) y = basetrans(x[0]);
+            }
+            return y;
+        }
+
+        string Addo(string so)
+        {
+            int ll = so.Length;
+            while (ll != 12)
+            {
+                so = "0" + so;
+                ++ll;
+            }
+            return so;
+        }
+
+        void NtoVie(string t)
+        {
+            string t2 = "";
+            string t3 = "";
+            int l, t1; l = t.Length;
+            int r = l / 3;
+            if (!eng)
+            {
+                if (l >= 3)
+                {
+                    t = Addo(t);
+                    l = t.Length;
+                    t1 = l - 1;
+                    t2 = t.Substring(t1 - 2, 3);
+
+                    if ((t2[0] == '0') && (t2 != "000"))
+                    {
+                        t3 = "không trăm ";
+                        if (t2[1] != '0')
+                            t2 = t3 + BaseN(t2);
+                        else
+                        if (t2 != "0") t2 = t3 + "lẻ " + BaseN(t2);
+                    }
+                    else t2 = BaseN(t2);
+                    t1 = t1 - 3;
+                    if ((r >= 1))
+                    {
+                        t3 = t.Substring(t1 - 2, 3);
+                        t3 = BaseN(t3);
+                        if (t3 != "") t2 = t3 + " ngàn " + t2;
+                        t1 = t1 - 3;
+                    }
+                    if ((r >= 2))
+                    {
+                        t3 = t.Substring(t1 - 2, 3);
+                        t3 = BaseN(t3);
+                        if (t3 != "") t2 = t3 + " triệu " + t2;
+                        t1 = t1 - 3;
+                    }
+                    if ((r >= 3))
+                    {
+                        t3 = t.Substring(t1 - 2, 3);
+                        t3 = BaseN(t3);
+                        if (t3 != "") t2 = t3 + " tỉ " + t2;
+                    }
+                }
+                else t2 = BaseN(t);
+            }
+            else
+            {
+                if (l >= 3)
+                {
+                    t = Addo(t);
+                    l = t.Length;
+                    t1 = l - 1;
+                    t2 = t.Substring(t1 - 2, 3);
+
+                    if ((t2[0] == '0') && (t2 != "000"))
+                    {
+                        if (t2[1] != '0')
+                            t2 = t3 + BaseN(t2);
+                        else
+                        if (t2 != "0") t2 = t3 + "and " + BaseN(t2);
+                    }
+                    else t2 = BaseN(t2);
+                    t1 = t1 - 3;
+                    if ((r >= 1))
+                    {
+                        t3 = t.Substring(t1 - 2, 3);
+                        t3 = BaseN(t3);
+                        if (t3 != "") t2 = t3 + " thousand " + t2;
+                        t1 = t1 - 3;
+                    }
+                    if ((r >= 2))
+                    {
+                        t3 = t.Substring(t1 - 2, 3);
+                        t3 = BaseN(t3);
+                        if (t3 != "") t2 = t3 + " million " + t2;
+                        t1 = t1 - 3;
+                    }
+                    if ((r >= 3))
+                    {
+                        t3 = t.Substring(t1 - 2, 3);
+                        t3 = BaseN(t3);
+                        if (t3 != "") t2 = t3 + " billion " + t2;
+                    }
+                }
+                else t2 = BaseN(t);
+            }
+            ReadNum.Text = t2;
+        }
+
+        private void Num_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox n = (TextBox)sender;
+
+            if (n.Text.Length >= 12)
+            {
+                if ((e.KeyChar == 8) || (e.KeyChar == 127) || (e.KeyChar == 13))
+                    e.Handled = false;
+                else e.Handled = true;
+            }
+            if (e.KeyChar == 13)
+            {
+                string k = Num.Text;
+                NtoVie(k);
+            }
+            else
+            if ((n.Text.Contains("1") || n.Text.Contains("2") ||
+                 n.Text.Contains("3") || n.Text.Contains("4") ||
+                 n.Text.Contains("5") || n.Text.Contains("6") ||
+                 n.Text.Contains("7") || n.Text.Contains("8") ||
+                 n.Text.Contains("9")) && (e.KeyChar == '0') &&
+                 (n.Text.Length < 12))
+                e.Handled = false;
+            else
+            if (((e.KeyChar >= 32) && (e.KeyChar < 49)) || (e.KeyChar > 57)) e.Handled = true;
+
+            if (n.Text.Length != 0)
+            {
+                if ((n.Text.Length == 1) && (n.Text[0] == '0'))
+                {
+                    ReadNum.Text = "không";
+                    n.Text = "";
+                }
+                else
+                if ((n.Text[0] == '0') && (n.Text.Length > 1))
+                {
+                    n.Text = Delo(n.Text);
+                }
+            }
+        }
+    
+        private void Eng_Click(object sender, EventArgs e)
+        {
+            eng = true;
+            if (Num.Text != "") NtoVie(Num.Text);
+            else ReadNum.Text = "";
+        }
+
+        private void Vie_Click(object sender, EventArgs e)
+        {
+            eng = false;
+            if (Num.Text != "") NtoVie(Num.Text);
+            else ReadNum.Text = "";
+        }
+
+        private void Audio_Click(object sender, EventArgs e)
+        {
+            string k = ReadNum.Text;
+            tmp++;
+            if (k != "")
+            {
+                if (eng) voice.SpeakAsync(k);
+                else
+                {
+                    var url = "https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=" + "vi" + "&q=" + k + "&rd=";
+                    url += HttpUtility.UrlEncode(k, Encoding.GetEncoding("utf-8"));
+                    using (var client = new WebClient())
+                    {
+                        client.DownloadFile(url,"speech" + tmp.ToString() + ".mp3");
+                    }
+
+                    WMPLib.WindowsMediaPlayer maspplayer = new WMPLib.WindowsMediaPlayer();
+                    maspplayer.URL = "speech" + tmp.ToString() + ".mp3";
+                    maspplayer.controls.play();
+                }
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            if (eng) MessageBox.Show("See you next time!");
+            else MessageBox.Show("Hẹn gặp lại bạn!");
+            Application.Exit();
+        }
+
+        private void HelpButton_Click(object sender, EventArgs e)
+        {
+            if (!eng)
+            {
+                helpbox.Visible = true;
+                var t = Task.Delay(2500);
+                t.Wait();
+                helpbox.Visible = false;
+            }
+            else
+            {
+                helpbox2.Visible = true;
+                var t = Task.Delay(2500);
+                t.Wait();
+                helpbox2.Visible = false;
+            }
+        }
+    }
+}
